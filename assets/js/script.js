7c572be766476002bd27eed27aa58f48b4d0d45b
LewisHammy <samkabir30@gmail.com>
2023-08-10 18:53:09 -0700

    new styling added side nav bar and top nav

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,9 @@
-async function fetchData() {
-    const url = 'https://sports-live-scores.p.rapidapi.com/baseball/live';
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': '8516b89c04msh14301adfc08cb2ap103260jsne97130bb1c31',
-            'X-RapidAPI-Host': 'sports-live-scores.p.rapidapi.com'
-        }
-    };
-
-    try {
-        const response = await fetch(url, options);
-        const result = await response.text();
-        console.log(result);
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-fetchData();
+// Needs to be fixed
+// smooth scrolling
+document.querySelectorAll('navbar ul li a').forEach(link => {
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+      const target = document.querySelector(link.getAttribute('href'));
+      target.scrollIntoView({ behavior: 'smooth' });
+    });
+  });
